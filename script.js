2936c2b7f4ca798aef81bc438fe3a5146838a1b0
Avery Dyer <avedyer@gmail.com>
2021-09-09 20:36:17 -0400

    implemented backend function of game.

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
-let choices = document.querySelectorAll('.playerChoice');
+let playerChoice;
+let computerChoice;
+let score = [0, 0];
+let options = ["rock", "paper", "scissors"];
+let outcome;
 
-for (choice of choices) {
-    choice.addEventListener('click', function(){
-        console.log(this);
+let playerOptions = document.querySelectorAll('.playerChoice');
+
+function computerPlay(){
+    let numChoice = Math.floor(Math.random() * 3);
+    return options[numChoice];
+}
+
+function playRound(){
+    let choices = [playerChoice, computerPlay()];
+
+    console.log(choices);
+
+    if (choices[0] === choices[1]){
+        return "It's a tie!";
+    }
+
+    else if (choices[0] === "rock"){
+        if (choices[1] === "paper"){
+            ++score[1]
+            return "Computer wins!";
+        }
+        ++score[0]
+        return "Player wins!"
+    }
+
+    else if(choices[0] === "paper"){
+        if(choices[1] === "rock"){
+            ++score[0]
+            return "Player wins!";
+        }
+        ++score[1]
+        return "Computer wins!"
+    }
+
+    else if(choices[1] === "rock"){
+        ++score[1]
+        return "Computer wins!";
+    }
+    ++score[0]
+    return "Player wins!"
+}
+
+for (playerOption of playerOptions) {
+    console.log(playerOption);
+    playerOption.addEventListener('click', function(){
+        playerChoice = this.value;
+        console.log(playRound());
+        console.log(score);
     });
 }
